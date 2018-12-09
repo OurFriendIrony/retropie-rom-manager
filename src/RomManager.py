f69d269f5d1764fb01a31222bcc4e0a314b97cce
Steve Stevens <steve.h.i.stevens@gmail.com>
2018-12-09 02:07:32 +0000

    rom manager - tidy output + start writing tests...

--- src/RomManager.py
+++ src/RomManager.py
@@ -14,26 +14,45 @@ def get_first_element(stdout):
         return 0
 
 
-bar_char = ">"
+bar_0 = "-"
+bar_1 = ">"
+
+state_copy_complete = "COPIED"
+state_copy_skipped = "SKIPPED"
+state_copy_not_required = "EXISTS"
 
 
 def progress(filename, size, sent):
     diff = (float(sent) / float(size) * 100)
-    bar = "{:-<20}".format(bar_char * int(diff / 5))
+    bar = "{:{b}<20}".format(bar_1 * int(diff / 5), b=bar_0)
 
     if int(diff) == 100:
-        print("\r| {:>20} | {:>10} | {}".format(bar, "DONE", filename))
+        print("\r | {:>20} | {:^10} | {}".format(bar, state_copy_complete, filename))
     else:
-        sys.stdout.write("\r| {:>20} | {:>9.02f}% | {}".format(bar, diff, filename))
+        sys.stdout.write("\r | {:>20} | {:^9.02f}% | {}".format(bar, diff, filename))
 
 
-class SaveManager:
+class RomManager:
     ip = "192.168.1.177"
     emus = [
         "atari2600", "atari7800", "nes", "snes",
         "megadrive", "gba", "n64", "dreamcast",
         "nds", "fba", "psx"
     ]
+
+    skip_roms = {
+        "atari2600": [],
+        "atari7800": [],
+        "nes": [],
+        "snes": [],
+        "megadrive": [],
+        "gba": [],
+        "n64": [],
+        "dreamcast": [],
+        "nds": [],
+        "fba": [],
+        "psx": []
+    }
 
     pc_roms_home = "/media/videos/Games"
     pi_roms_home = "/home/pi/RetroPie/roms"
@@ -49,7 +68,7 @@ class SaveManager:
             self.print_emu_header(emu)
 
             games_dir = "{}/{}/games/".format(self.pc_roms_home, emu)
-            game_files = os.listdir(games_dir)
+            game_files = sorted(os.listdir(games_dir))
 
             for game_file in game_files:
                 rom_from = self.pc_roms_home + "/" + emu + "/games/" + game_file
@@ -58,13 +77,27 @@ class SaveManager:
                 rom_from_size = self.get_local_filesize(rom_from)
                 rom_to_size = self.get_remote_filesize(rom_to)
 
-                if rom_from_size == rom_to_size:
-                    print("| {:>20} | {:>10} | {}".format(bar_char * 20, "OK", game_file))
+                if self.is_skipped_rom(emu, game_file):
+                    print("\r | {:{b}>20} | {:^10} | {}".format(bar_0 * 20, state_copy_skipped, game_file, b=bar_0))
+                elif rom_from_size == rom_to_size:
+                    print("\r | {:{b}>20} | {:^10} | {}".format(bar_1 * 20, state_copy_not_required, game_file, b=bar_0))
                 else:
-                    # print("{:>10} -> {:>10} | {}".format(rom_to_size, rom_from_size, game_file))
-                    self.ssh.exec_command("rm \"{}\"".format(rom_to))
-                    self.scp.put(rom_from, rom_to)
+                    self.copy_rom(rom_from, rom_to)
+
             print("")
+
+    def copy_rom(self, rom_from, rom_to):
+        # 'progress' function prints output from this operation
+        self.ssh.exec_command("rm \"{}\"".format(rom_to))
+        self.scp.put(rom_from, rom_to)
+
+    def is_skipped_rom(self, emu, rom):
+        rom_raw = os.path.splitext(rom)[0]
+        skip_roms_emu = self.skip_roms.get(emu, [])
+        if "*" in skip_roms_emu:
+            return True
+        else:
+            return rom_raw in skip_roms_emu
 
     def print_emu_header(self, emu):
         ll = 100
@@ -79,4 +112,4 @@ class SaveManager:
 
 
 if __name__ == '__main__':
-    SaveManager()
+    RomManager()
