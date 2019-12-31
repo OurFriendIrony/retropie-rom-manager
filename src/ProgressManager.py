b120c44d73d22ad2bd4023426b4e2d9f80dfdea5
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-12-31 18:30:54 +0000

    remove hardcoding lengths

--- src/ProgressManager.py
+++ src/ProgressManager.py
@@ -13,14 +13,16 @@ def _str(data):
 
 
 class ProgressManager:
-    bar_h = "_"
-    bar_wp = 20
-    bar_wh = 100
+    CHAR_HEADER     = "_"
+    WIDTH_PROGRESS  = 45
+    WIDTH_STATUS    = 10
+    WIDTH_STATUS_F  = str(WIDTH_STATUS-1)+'.02f'
+    WIDTH_HEADER    = 100
 
-    COPIED = "-Copied-"
-    BACKEDUP = "-Backedup-"
-    RESTORED = "-Restored-"
-    SKIPPED = "-Ignored-"
+    COPIED       = "-Copied-"
+    BACKEDUP     = "-Backedup-"
+    RESTORED     = "-Restored-"
+    SKIPPED      = "-Ignored-"
     NOT_REQUIRED = "-"
 
     def __init__(self, end_state=None):
@@ -32,46 +34,66 @@ class ProgressManager:
     def print_emu_header(self, emu, subheader=None):
         if subheader:
             print("__{:^{ll}}__\n| {:^{ll}} |\n'.{:^{ll}}.'".format(
-                self.bar_h * self.bar_wh,
+                self.CHAR_HEADER * self.WIDTH_HEADER,
                 "{} [{}]".format(emu.upper(), subheader),
-                self.bar_h * self.bar_wh,
-                ll=self.bar_wh
+                self.CHAR_HEADER * self.WIDTH_HEADER,
+                ll=self.WIDTH_HEADER
             ))
         else:
             print("__{:^{ll}}__\n| {:^{ll}} |\n'.{:^{ll}}.'".format(
-                self.bar_h * self.bar_wh,
+                self.CHAR_HEADER * self.WIDTH_HEADER,
                 emu.upper(),
-                self.bar_h * self.bar_wh,
-                ll=self.bar_wh
+                self.CHAR_HEADER * self.WIDTH_HEADER,
+                ll=self.WIDTH_HEADER
             ))
 
     def print_action_skipped(self, filename):
-        print("\r |{:^20}| {:^10} | {}".format(
-            "n/a",
-            self.SKIPPED,
-            filename
-        ))
+        w1=str(self.WIDTH_PROGRESS)
+        w2=str(self.WIDTH_STATUS)
+        line="\r |{:^"+w1+"}| {:^"+w2+"} | {}"
+        print(
+            line.format(
+                "n/a",
+                self.SKIPPED,
+                filename
+            )
+        )
 
     def print_action_not_required(self, filename):
-        print("\r |{:^20}| {:^10} | {}".format(
-            self.get_bar(100.0),
-            self.NOT_REQUIRED,
-            filename
-        ))
+        w1=str(self.WIDTH_PROGRESS)
+        w2=str(self.WIDTH_STATUS)
+        line="\r |{:^"+w1+"}| {:^"+w2+"} | {}"
+        print(
+            line.format(
+                self.get_bar(100.0),
+                self.NOT_REQUIRED,
+                filename
+            )
+        )
 
     def print_action_complete(self, filename):
-        print("\r |{:^20}| {:^10} | {}".format(
-            self.get_bar(100.0),
-            self.end_state,
-            filename
-        ))
+        w1=str(self.WIDTH_PROGRESS)
+        w2=str(self.WIDTH_STATUS)
+        line="\r |{:^"+w1+"}| {:^"+w2+"} | {}"
+        print(
+            line.format(
+                self.get_bar(100.0),
+                self.end_state,
+                filename
+            )
+        )
 
     def print_action_progress(self, diff, filename):
-        sys.stdout.write("\r |{:^20}| {:^9.02f}% | {}".format(
-            self.get_bar(diff),
-            diff,
-            filename
-        ))
+        w1=str(self.WIDTH_PROGRESS)
+        w2=str(self.WIDTH_STATUS_F)
+        line="\r |{:^"+w1+"}| {:^"+w2+"}% | {}"
+        sys.stdout.write(
+            line.format(
+                self.get_bar(diff),
+                diff,
+                _str(filename)
+            )
+        )
 
     def progress(self, filename, size, sent):
         diff = (float(sent) / float(size) * 100)
@@ -80,10 +102,12 @@ class ProgressManager:
         else:
             self.print_action_progress(diff, filename)
 
-    def get_bar(self, diff):
-        len_1 = int(diff / 5)
-        len_2 = 1 - int(diff / self.bar_wh)
-        return "{:20}".format(("=" * len_1) + (">" * len_2))
+    def get_bar(self,diff):
+        w1=str(self.WIDTH_PROGRESS)
+        line="{:"+w1+"}"
+        len_1 = int(diff * self.WIDTH_PROGRESS / 100)
+        len_2 = 1 - int(diff / self.WIDTH_HEADER)
+        return line.format(("=" * len_1) + (">" * len_2))
 
 
 if __name__ == '__main__':
